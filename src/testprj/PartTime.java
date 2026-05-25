2c28151c5964e64f691957f5aa9afaac445b4754
sivagamasrinivasan <784108@pdsb.net>
2026-05-25 10:17:49 -0400

    branch - new class added

---
+++ src/testprj/PartTime.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testprj;
+
+/**
+ *
+ * @author sivag
+ */
+public class PartTime {
+    private int semester;
+
+    /**
+     * @return the semester
+     */
+    public int getSemester() {
+        return semester;
+    }
+
+    /**
+     * @param semester the semester to set
+     */
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+}
